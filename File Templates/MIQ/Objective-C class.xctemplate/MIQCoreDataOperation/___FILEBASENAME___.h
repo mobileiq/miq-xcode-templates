d763a2fe3868b8d5bbc342fb477d3936ec735512
David Hardiman <dhardiman@mobileiq.com>
2011-04-28 15:22:26 +0100

    MIQCoreDataOperation subclass

---
+++ File Templates/MIQ/Objective-C class.xctemplate/MIQCoreDataOperation/___FILEBASENAME___.h
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MIQCoreDataOperation"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : MIQCoreDataOperation {
+    
+}
+
+@end
