69f839d77677632e4e10b210b6e0a29d16bd4ba7
David Hardiman <dhardiman@mobileiq.com>
2011-04-28 15:18:05 +0100

    Initial commit

---
+++ File Templates/MIQ/Objective-C class.xctemplate/UITableViewCell/___FILEBASENAME___.m
@@ -0,0 +1,31 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___FILEBASENAME___.h"
+
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if ((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+@end
