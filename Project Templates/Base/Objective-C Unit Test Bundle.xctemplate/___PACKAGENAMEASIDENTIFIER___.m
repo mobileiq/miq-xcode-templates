db512331e5c4c5cc52ce057504998d058743e33c
David Hardiman <dhardiman@mobileiq.com>
2011-05-15 10:41:25 +0100

    Initial Mobile IQ project templates

---
+++ Project Templates/Base/Objective-C Unit Test Bundle.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -0,0 +1,37 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : SenTestCase {
+    
+}
+
+@end
+
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)setUp {
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown {
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample {
+    STFail(@"Unit tests are not implemented yet in ___PACKAGENAME___");
+}
+
+@end
