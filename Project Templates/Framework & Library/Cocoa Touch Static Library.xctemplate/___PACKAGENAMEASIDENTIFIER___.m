f4f2f4ac72c31c4f577424e9f21e43333eaa2542
David Hardiman <dhardiman@mobileiq.com>
2011-12-07 11:12:13 +0000

    Initial 4.2 project templates

---
+++ Project Templates/Framework & Library/Cocoa Touch Static Library.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id)init {
+    if ((self = [super init])) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
