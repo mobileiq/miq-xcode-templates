36fbae68b186ccafa85a7e310f623c07171ba771
David Hardiman <dhardiman@mobileiq.com>
2012-10-31 12:08:14 +0000

    Add sentest case templates

---
+++ File Templates/MIQ/Objective-C test case class.xctemplate/SenTestWithSubclass/___FILEBASENAME___.m
@@ -0,0 +1,12 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+
+TEST_CASE_WITH_SUBCLASS(___FILEBASENAMEASIDENTIFIER___, ___VARIABLE_cocoaTouchSubclass___)
+
+END_TEST_CASE
